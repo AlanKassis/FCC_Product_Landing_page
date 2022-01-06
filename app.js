c847f8ce175e9fedc2be04c7ead3d9f69ddde593
Alan Kassis <Alankassis@gmail.com>
2022-01-06 19:20:17 +1000

    finished writing html + css

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 var express = require('express');
 var path = require('path');
-
+// const nm_dependencies = ['@fortawesome'];
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+// nm_dependencies.forEach(dep => {
+//   app.use(`/${dep}`, express.static(path.resolve(`node_modules/${dep}`)));
+// })
 
 // view engine setup
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 })
+
+
 let port = process.env.PORT;
 if (port == null || port =="") {
   port = 8000;
